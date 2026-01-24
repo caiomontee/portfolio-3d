5f8943eb6fd4fb1065b56dae6b7c23fd2cd9a0ec
Caio Monte <caiomontebelo@gmail.com>
2026-01-23 22:18:09 -0300

    Fix: corrigindo import do Globe para maiusculo

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import CopyEmailButton from "../components/CopyEmailButton";
 import { Frameworks } from "../components/Frameworks";
-import { Globe } from "../components/Globe";
+import { Globe } from "../components/globe";
 
 const About = () => {
   const grid2Container = useRef();
